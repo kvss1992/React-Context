31edce7f9de55b5678f20dc3c401fbc2d08fa201
Karanvir Sawhney <kvss1992@users.noreply.github.com>
2019-02-14 23:09:14 -0500

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
+//  context
+const MyContext = React.createContext();
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+// Context Provider
+class MyProvider extends React.Component {
+  state = {
+    name: "troller",
+    age: 21,
+    cool: true
+  };
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          growAYearOlder: () =>
+            this.setState({
+              age: this.state.age + 1
+            })
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+//  Family
+const Family = props => (
+  <div className="family">
+    <Person />
+  </div>
+);
+
+//  Person
+class Person extends React.Component {
+  render() {
+    return (
+      <div className="parent">
+        <MyContext.Consumer>
+          {context => (
+            <React.Fragment>
+              <p>name: {context.state.name}</p>
+              <p>age: {context.state.age}</p>
+              <button onClick={context.growAYearOlder}> Add an Year!!</button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <MyProvider>
+        <div>
+          <p>I am a family</p>
+          <Family />
+        </div>
+      </MyProvider>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
